d907337486b2a303e3c3a71f3c35b1a6d9fcb2a6
Kaldr <huangyukaldr@live.cn>
2021-04-23 21:45:40 +0800

    添加股票信息查询代码

--- common/database/stock.py
+++ common/database/stock.py
@@ -3,6 +3,11 @@ from common.database.config import uri
 
 client = MongoClient(uri)
 stock_db = client.get_database('stock_liuyao')
+tushare_info = {
+    "token": "c52c51a4cde56aa199146a3a6abe6e11d2a8607556c014b4ace4f4c5",
+    'username': '18618124986',
+    "password": 'ivy2498'
+}
 
 stock_info_collection = stock_db.get_collection("stock_info")
 stock_daily_kline_collection = stock_db.get_collection("stock_daily_kline")
--- liuyao/stock_prediction/MGYD/data_parser.py
+++ liuyao/stock_prediction/MGYD/data_parser.py
@@ -1,7 +1,9 @@
 import os
 import csv
-from common.database.stock import stock_info_collection, stock_daily_kline_collection
+from common.database.stock import stock_info_collection, stock_daily_kline_collection, tushare_info
 from datetime import datetime
+import baostock
+import tushare
 
 
 class DataParser:
@@ -15,10 +17,42 @@ class DataParser:
 
     def get_stock_info(self):
         """
-        获取每一个股票的信息
+        获取股票的信息
         :return:
         """
-        pass
+        result = {}
+        n = len(self.stocks)
+        baostock.login()
+        # tushare.set_token(tushare_info['token'])
+        # pro = tushare.pro_api()
+        i = 0
+        for stock in self.stocks:
+            rs = baostock.query_stock_basic("%s.%s" % (stock[1], stock[2]))
+            # nc = pro.namechange(ts_code='%s.%s' % (stock[2], stock[1]))
+            # print(nc)
+            # 打印结果集
+            data_list = []
+            while (rs.error_code == '0') & rs.next():
+                # 获取一条记录，将记录合并在一起
+                data_list.append(rs.get_row_data())
+            if data_list:
+                stock_info = data_list[0]
+                data = {
+                    "stock": stock[0],
+                    'bao_stock_id': stock_info[0],
+                    'stock_name': stock_info[1],
+                    'ipo_date': datetime.strptime(stock_info[2], "%Y-%m-%d"),
+                    'out_date': None if not stock_info[3] else None,
+                    'type': int(stock_info[4]),
+                    'status': int(stock_info[5]),
+                    'name_history': []
+                }
+                stock_info_collection.update_one({"stock": data['stock']}, data, upsert=True)
+            i += 1
+            print("%s-%s/%s" % (stock[0], i, n))
+            break
+        baostock.logout()
+        return result
 
     def get_stock_from_path(self):
         """
