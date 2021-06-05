30ca22756cb71e653cb5112476ed1268fe14adfe
kaldr <huangyukaldr@live.cn>
2021-06-05 18:34:32 +0800

    学习一些断语

--- liuyao/common/prediction/strength.py
+++ liuyao/common/prediction/strength.py
@@ -2,6 +2,15 @@ class Strength:
     pass
 
 
+class PoStrength:
+    """
+    化破，有病，不能截煞。
+    截杀的前提是旺相。
+
+    """
+    pass
+
+
 class ChongStrength:
     pass
 
@@ -23,4 +32,24 @@ class XingStrength:
 
 
 class HaiStrength:
+    pass
+
+
+class MuStrength:
+    """
+    化墓：动化墓处于囚态，不得自由，动爻无释放权。
+    动墓：有墓像，对旺衰的影响不大。
+    日暮：静爻在日论入墓，影响旺衰。但是动爻在日墓，对旺衰影响不大，但是要取像。静爻日暮得传生，不再论日暮。例如亥水辰日得酉金发动，不再论日暮。
+    月墓：有墓像，没有作用。
+    """
+    pass
+
+
+class KongStrengtch:
+    """
+    空亡受生受克的程度小，但是也受作用。
+    静空逢克境都是真空,
+    如果发动还有气，就不能说是真空了。
+
+    """
     pass
