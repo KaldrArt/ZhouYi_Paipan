e4b8f8c6d82bd04589ce4f4dcd8cb21d2344930b
kaldr <huangyukaldr@live.cn>
2021-08-20 00:43:26 +0800

    添加卦和新派断法

--- xinpai/gan_zhi_wang_ruo/wang_ruo.py
+++ xinpai/gan_zhi_wang_ruo/wang_ruo.py
@@ -1,5 +1,16 @@
-class WangRuo:
+from common.relations.relation import Relation
+from common.yi_base import Yi
+
+
+class XinPaiRelation(Relation):
+    def __init__(self, target_gan_zhi: Yi, effect_gan_zhi: Yi):
+        pass
+
+
+class WangRuo(XinPaiRelation):
     """
     判断干支旺弱的基础程序
     """
-    pass
+
+    def __init__(self, target_gan_zhi: Yi, effect_gan_zhi: Yi):
+        pass
